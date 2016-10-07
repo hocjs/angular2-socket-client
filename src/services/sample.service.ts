b3326506cf113527bce4fe356cb43850fb154c73
Dyam Marcano <dyam.marcano@gmail.com>
2016-10-07 13:21:08 -0400

    first commit

--- src/services/sample.service.ts
+++ src/services/sample.service.ts
@@ -1,9 +1,37 @@
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import * as io from 'socket.io-client';
 
 @Injectable()
-export class SampleService {
+export class SocketService {
+
+  private url : string;
+  private socket : any;
 
   constructor() {
 
+    this.socket = io(this.url);
+
+    this
+      .on('connect')
+      .subscribe(() => {
+        console.log(`connected! id: ${this.socket.id}`);
+      });
+
+    this
+      .on('disconnect')
+      .subscribe(() => {
+        console.log(`disconnected! id: ${this.socket.id}`);
+      });
+  }
+
+  public on(event : string) {
+    return Observable.fromEvent(this.socket, event);
+  }
+
+  public emit(event : string, value : Object) {
+    this
+      .socket
+      .emit(event, value);
   }
 }
